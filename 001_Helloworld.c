1dc9226e2a1e869efdc6f874df077b0ddaea51c6
charantejabeera <charanbeera303@gmail.com>
2024-06-07 05:29:00 -0700

    Add files via upload

---
+++ 001_Helloworld.c
@@ -0,0 +1,19 @@
+/*
+Program name: 001_Helloworld.c
+Objective: Displays Hello world
+Author: Charanteja Beera
+Date: 7th June 2024
+*/
+# include <stdio.h>
+
+main() //indicates starting point of the program execution
+
+{printf("Hello World......");
+ printf("\n");
+
+
+
+
+
+getch();
+}
