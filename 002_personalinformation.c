b72e05c305026c2807960aa487f67b33a7161deb
charantejabeera <charanbeera303@gmail.com>
2024-06-07 08:52:15 -0700

    Add files via upload

---
+++ 002_personalinformation.c
@@ -0,0 +1,20 @@
+/*program name : 002_personalinformation .c
+ objective: To display personal information of me
+ author : Charan teja beera
+ Date: 8th june 2024*/
+
+# include <stdio.h>
+
+  main() //indicates the starting point of the program execution
+
+  {
+      printf("Name:Charanteja Beera");
+      printf("\nDOB:24 Jan");
+      printf("\nMobile:123456789");
+
+
+
+
+      getch();
+
+  }
